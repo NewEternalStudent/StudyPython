09955deb6db078574a2a4ffae0e54ab9a366d2a8
Ilin Evgeny <ilinei52@gmail.com>
2019-01-16 23:53:24 +0300

    first commit

---
+++ ComparisonOperators/main.py
@@ -0,0 +1,24 @@
+print(6>5)
+print(type(True));
+print(0.1 + 0.1)
+print(0.1 + 0.1 + 0.1)
+print (2>4)
+print (45>3)
+print (2>4 and 45>3)
+print (2>4 or 45>3)
+print('' and 2)  #false and true return - ''
+print('' or 2) #false or true return - 2
+
+#---------------------------------
+y = 6>8
+print ('y = 6>8:',y)
+print ('not y:', not y)
+print ('not None:', not None)
+print ('not 2:', not 2)
+
+
+print ('3 and 5:', 3 and 5)
+print ('0 and 5', 0 and 5)
+print ('\'\' and 5', '' and 5)
+
+print ( (7>3) + 1 ) #false = 0 true = 1
